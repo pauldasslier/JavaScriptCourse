df47c97e1e645e1a6337d5117d0ca2ad59d10813
Pavel <adasslier@mail.ru>
2018-08-17 23:06:16 +0300

    Это десятый коммит

--- lesson 5/additional/script.js
+++ lesson 5/additional/script.js
@@ -10,10 +10,57 @@ function formatDate() {
 
 	if (month < 10) {
 		month = '0' + month;
-	} else if (date < 10) {
-		date = '0' + date;
 	}
-	console.log(date);
+	if (date < 10) {
+		date = '0' + date;
+	} 
+	if (hours < 10) {
+		hours = '0' + hours;
+	} 
+	if (minutes < 10) {
+		minutes = '0' + minutes;
+	}
+	if (seconds < 10) {
+		seconds = '0' + seconds;
+	}
+
+console.log(hours + ':' + minutes + ':' + seconds + ' ' + date + '.'
+		+ month + '.' + year);
+}
+formatDate();
+
+
+function weekDay() {
+	let week = [
+ 'понедельник',
+ 'вторник',
+ 'среда',
+ 'четверг',
+ 'пятница',
+ 'суббота',
+ 'воскресенье'
+ ]
+
+ let day = now.getDay() + 1;
+ 
+ for(let i = 0; i < week.length; i++) {
+ 	if (i == day - 1) {
+ 		console.log(week[i]);
+ 	}
+ }
 }
 
-formatDate();
+weekDay();
+
+let div = document.getElementById('result');
+
+function difference() {
+	let x = document.getElementById('first').value;
+	let y = document.getElementById('second').value;
+
+	let result = (Math.abs(new Date(x).getTime() - new Date(y).getTime())/1000/60/60/24);
+	if (typeof(result) == 'number') {
+		div.innerHTML = result;
+	}
+}
+document.getElementById('btn').onclick = difference;
