4d67a5935aab9be2792f96a72337acc12a86d414
Pavel <adasslier@mail.ru>
2018-09-08 01:06:28 +0300

    Это двадцать восьмой коммит

---
+++ diploma/js/parts/modal_design.js
@@ -0,0 +1,25 @@
+function modalDesign() {
+	let btnDesign = document.getElementsByClassName('button-design'),
+	    close = document.getElementsByClassName('popup-close')[2],
+	    modalForm = document.querySelector('.popup-design');
+
+	function showModal(prop) {
+		for(let i = 0; i < btnDesign.length; i++) {
+			btnDesign[i].addEventListener('click', function() {
+				modalForm.style.display = 'block';
+			});
+			close.addEventListener('click', function() {
+				modalForm.style.display = 'none';
+			});
+			modalForm.addEventListener('click', function() {
+				if (event.target == modalForm) {
+					modalForm.style.display = 'none';
+				}
+			})
+		}
+	}
+
+	showModal();
+}
+
+module.exports = modalDesign;
--- diploma/js/script.js
+++ diploma/js/script.js
@@ -3,8 +3,10 @@ window.addEventListener('DOMContentLoaded', function() {
 
   let sliderTop = require('./parts/slider_top.js');
   let sliderBottom = require('./parts/slider_bottom.js');
+  let modalDesign = require('./parts/modal_design.js');
 
   sliderTop();
   sliderBottom();
+  modalDesign();
 
 });
