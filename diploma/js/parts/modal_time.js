764be747bf1134a9a123ebd88279db468849fbda
Pavel <adasslier@mail.ru>
2018-09-09 02:06:15 +0300

    Это тридцать первый коммит

--- diploma/js/parts/modal_time.js
+++ diploma/js/parts/modal_time.js
@@ -1,18 +1,24 @@
 function modalTime() {
 	let modalForm = document.querySelector('.popup-consultation'),
 	    designForm = document.querySelector('.popup-design'),
-	    time = Math.floor(performance.now() / 1000);
+	    giftForm = document.querySelector('.popup-gift');
 
-	    console.log(time);
+	// Запускаем интервал с проверкой раз в 1 сек. для получения времени пребывания на странице
+	let timerId = setInterval(function() {
+		 // При превышении времени на странице в 60к миллисекунд - выводим модальное окно
+	  if (performance.now() > 60000) {
+	  	if (designForm.style.display != 'block' && modalForm.style.display != 'block' && 
+	  		giftForm.style.display != 'block') {
+	  		modalForm.style.display = 'block';
+	  	}
+	  }
+	}, 1000);
 
-	// function showModal() {
-	// 	if (designForm.style.display != 'block' && designForm.style.display == '') {
-	// 		modalForm.style.display = 'block';
-	// 	}
-	// }
-
- // setTimeout(showModal, 5000)
+ // Очищаем интревал через 40 миллисекунд, чтобы пользователь не вызвал
+ // 2ое модальное окно подряд и для освобождения памяти
+	setTimeout(function() {
+	  clearInterval(timerId);
+	}, 60040);
 
 }
-
 module.exports = modalTime;
