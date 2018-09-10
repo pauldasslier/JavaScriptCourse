a9261991a293a0966386ec65c39f00f9e769da25
Pavel <adasslier@mail.ru>
2018-09-10 21:18:14 +0300

    Это тридцать третий коммит

--- diploma/js/parts/load_img.js
+++ diploma/js/parts/load_img.js
@@ -4,10 +4,13 @@ function loadImg() {
 
 	loadBtn.addEventListener('click', function() {
 		for(let i = 0; i < hiddenStyles.length; i++) {
-			hiddenStyles[i].style.setProperty ('display', 'block', 'important');
+			if (hiddenStyles[i].classList.contains('styles-2')) {
+				hiddenStyles[i].classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
+				hiddenStyles[i].classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+			}
 		}
 		this.remove(loadBtn);
-	})
+	});
 }
 
 module.exports = loadImg;
