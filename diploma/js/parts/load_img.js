9b4000f3b07753dcc2c104fd2c0069150c9c05eb
Pavel <adasslier@mail.ru>
2018-09-12 03:36:15 +0300

    Это тридцать пятый коммит

--- diploma/js/parts/load_img.js
+++ diploma/js/parts/load_img.js
@@ -9,7 +9,7 @@ function loadImg() {
 				hiddenStyles[i].classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
 			}
 		}
-		this.remove(loadBtn);
+		loadBtn.style.display = 'none';
 	});
 }
 
